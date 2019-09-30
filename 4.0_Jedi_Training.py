0efdc4832c54979dc8e371fb7627a4bcb013fdbc
leavertonnel <leavertonnel@urbandaleschools.com>
2019-09-30 14:15:05 -0500

    Final commit

--- 4.0_Jedi_Training.py
+++ 4.0_Jedi_Training.py
@@ -12,8 +12,10 @@
 # 2. Make the following program work.
      
 # x = input("Enter a number: ")
-# if x : 3
-# print("You entered 3")
+# if x == "3":
+#     print("You entered 3")
+# else:
+#     print("You didn't print a 3")
 
 
  #  # 3. Make the following program work.
@@ -28,32 +30,36 @@
  #  # 4. Make the following program work.
 
 # jedi = input("Name one of the top 3 greatest Jedi.")
-# if jedi == "Yoda" or "Luke Skywalker" or "Obi-Wan Kenobi":
-#     print ("That is correct!")
+#
+# if jedi.lower() == "yoda":
+#     print("That is correct!")
+# elif jedi.lower() == "luke skywalker":
+#     print("That is correct")
+# elif jedi.lower() == "Obi-Wan Kenobi":
+#     print("That is correct")
+# else:
+#     print("please try again! That's not right")
 
 
 # 5. Make the following program work whether they enter a, A, Jedi Master or jedi master
 #    Print "Not a choice!" if they don't choose any of the three and set sensitivity to blank text.
 
-print("Welcome to the Jedi Academy!")
-
-print("A. Jedi Master")
-print("B. Sith Lord")
-print("C. Droid\n")
-
-user_input = input("Choose a character?\n")
-
-if user_input == "A" or user_input.lower() == "a":
-    sensitivity = 1000
-elif user_input == "B" or user_input.lower() == "b":
-    sensitivity = 900
-elif user_input == "C" or user_input.lower() == "c":
-    sensitivity = 0
-else:
-    sensitivity = (" ")
-    print("Not a choice!\n")
-
-print("Sensitivity: ", sensitivity )
-
-
-
+# print("Welcome to the Jedi Academy!")
+#
+# print("A. Jedi Master")
+# print("B. Sith Lord")
+# print("C. Droid\n")
+#
+# user_input = input("Choose a character?\n")
+#
+# if user_input == "A" or user_input.lower() == "a":
+#     sensitivity = 1000
+# elif user_input == "B" or user_input.lower() == "b":
+#     sensitivity = 900
+# elif user_input == "C" or user_input.lower() == "c":
+#     sensitivity = 0
+# else:
+#     sensitivity = (" ")
+#     print("Not a choice!\n")
+#
+# print("Sensitivity: ", sensitivity )
